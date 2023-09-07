0b24be06731f7e173f370c78daf839146c897c84
omargamil99 <143654003+omargamil99@users.noreply.github.com>
2023-09-07 16:23:35 +0200

    Learned about IfElse

---
+++ P9IfElse/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("What is your age?");
+int age = int.Parse(Console.ReadLine());
+
+string agegroup; 
+if (age <= 12)
+{
+    agegroup = "child";
+}
+else if (age <= 19) 
+{
+    agegroup = "teenager";
+}
+else
+{
+    agegroup = "grownup";
+}
+
+Console.WriteLine($"you are a {agegroup}" );
+
