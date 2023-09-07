5771266e529b4d704c7dc987a1252ddd9709df01
omargamil99 <143654003+omargamil99@users.noreply.github.com>
2023-09-07 14:07:41 +0200

    learned about bool

---
+++ P8Boolean/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("please enter your age");
+int age = int.Parse(Console.ReadLine());
+
+bool isChild = age >= 0 && age <= 12; 
+bool isTeenager = age >= 13 && age <= 19;
+bool isGrownup = age > 19; 
+
+Console.WriteLine("you are a child " + isChild);
+Console.WriteLine("you are a teenager" + isTeenager);
+Console.WriteLine("you are a grownup" + isGrownup);
